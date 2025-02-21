fb5f550550c0057709647f0dfe69b0d61fbcae83
kroto <dragon0549610@yahoo.co.jp>
2025-02-21 19:35:29 +0900

    defined health route

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Reveal health status on /up and /health that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+  get "health" => "rails/health#show"
 
   # Files API endpoints
   get 'files', to: 'files#index'
